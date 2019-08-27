a789345c0b6be4ee4fd4a48a03dfde3e6e724d71
loui7 <louislb99@gmail.com>
2019-08-27 10:14:41 +1000

    Complete 02_largestNumber

--- 02_largestNumber.js
+++ 02_largestNumber.js
@@ -11,8 +11,13 @@ with no changes to the function?
 */
 
 function largestNumber(arr) {
-    // Your code here
-    // Google JS syntax ;)
+    let biggest = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > biggest) {
+            biggest = arr[i];
+        }
+    }
+    return biggest;
 }
 
 var assert = require('assert');
