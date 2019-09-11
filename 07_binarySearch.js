fe201f9397610db12b3571feb363b1fee8958253
Louis Lloyd-Besson <louislb@eante.com.au>
2019-09-11 10:11:09 +1000

    finish 07_binarySearch

--- 07_binarySearch.js
+++ 07_binarySearch.js
@@ -1,23 +1,43 @@
 /*
-Write a method which will act as a binary search which will find the 
-position and the actual number of steps required to find the position. 
+Write a method which will act as a binary search which will find the
+position and the actual number of steps required to find the position.
 When the array has an even number of values the midpoint index will be rounded up.
 
 Example:
     binaryArray = [1,5,8,12,20,21,35]
     searchValue = 8
 
-    In this case the index returned would be 2 and it should take 3 steps. 
-    In the first step, 3 is the midpoint index (value = 12). 
-    In the second step 1 is the midpoint index (value = 5). 
+    In this case the index returned would be 2 and it should take 3 steps.
+    In the first step, 3 is the midpoint index (value = 12).
+    In the second step 1 is the midpoint index (value = 5).
     In the third and final step we are only left with 8 at index 2.
 
 */
 
 function binarySearch(binaryArray, searchValue) {
-    // Your code here
+  let staging = [...binaryArray];
+  let searchedIndex;
+  let lowInd = 0;
+  let steps = 0;
+  while (searchedIndex === undefined) {
+    const mid = Math.floor(staging.length / 2);
+    const midValue = staging[mid];
+    console.log(staging, 'lowInd', lowInd, 'ind:', mid, 'val:', midValue);
+    if (searchValue === midValue) {
+      searchedIndex = lowInd + mid;
+    }
+    if (searchValue < midValue) {
+      staging.splice(mid);
+    } else {
+      lowInd += mid;
+      staging = staging.splice(mid);
+    }
+    steps += 1;
+  }
+
+  return [searchedIndex, steps];
 }
 
 module.exports = {
-    binarySearch
+  binarySearch,
 };
