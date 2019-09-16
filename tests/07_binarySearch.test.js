63b48a34e98a6974ae0c56568e5a84409009171b
Louis Lloyd-Besson <louislb@eante.com.au>
2019-09-16 10:29:08 +1000

    linting

--- tests/07_binarySearch.test.js
+++ tests/07_binarySearch.test.js
@@ -1,16 +1,16 @@
-const binarySearch = require('../07_binarySearch').binarySearch;
+const { binarySearch } = require('../07_binarySearch');
 
-describe('Count loops', function () {
-    test('Should count one step when search values is in the middle', function () {
-        expect(binarySearch([1, 3, 7, 10, 14, 19, 31], 10)).toEqual([3, 1]);
-    })
-    test('Should count one step when search value is only value', function () {
-        expect(binarySearch([1], 1)).toEqual([0, 1])
-    })
-    test('Should count length divided by two steps when value is at beginning', function () {
-        expect(binarySearch([1, 3, 7, 10, 14, 19, 31], 1)).toEqual([0, 3]);
-    })
-    test('Should count length divided by two steps when value is at end', function () {
-        expect(binarySearch([1, 3, 7, 10, 14, 19, 31], 31)).toEqual([6, 3]);
-    })
-})
+describe('Count loops', () => {
+  test('Should count one step when search values is in the middle', () => {
+    expect(binarySearch([1, 3, 7, 10, 14, 19, 31], 10)).toEqual([3, 1]);
+  });
+  test('Should count one step when search value is only value', () => {
+    expect(binarySearch([1], 1)).toEqual([0, 1]);
+  });
+  test('Should count length divided by two steps when value is at beginning', () => {
+    expect(binarySearch([1, 3, 7, 10, 14, 19, 31], 1)).toEqual([0, 3]);
+  });
+  test('Should count length divided by two steps when value is at end', () => {
+    expect(binarySearch([1, 3, 7, 10, 14, 19, 31], 31)).toEqual([6, 3]);
+  });
+});
